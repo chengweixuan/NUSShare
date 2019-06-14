238bdcabc30e078444872ea4f7ae26a17804a3b1
Nicholas Cheong <nicholas1cheong@gmail.com>
2019-06-14 16:11:08 +0800

    stage 2

--- client/js/posts.js
+++ client/js/posts.js
@@ -6,6 +6,18 @@ Template.jokes.helpers({//to retrieve jokes from Jokes database
     var subbedthreads = Meteor.user().profile.subscriptions;
     var posts = Posts.find({chosenThread:{$in: subbedthreads}},{sort: {createdAt:-1}});
     return posts;
+  },
+  subscriptions: function(){
+    var subbedthreads = Meteor.user().profile.subscriptions;
+    var subscriptions = Threads.find({_id: {$in: subbedthreads}},{sort: {createdAt:-1}});
+    return subscriptions;
+  }
+});
 
+Template.jokes.events({
+  "click .btn": function(){
+  //console.log("filter button pressed");
+  var selectedThread = event.value;
+  console.log(selectedThread);
   }
 });

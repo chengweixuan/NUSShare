52dda047558b698f9b11589fab1e1eab791c57fa
Cheng Weixuan <exweixuan@gmail.com>
2019-06-16 18:09:11 +0800

    # filled out threadpage

--- client/js/threadpage.js
+++ client/js/threadpage.js
@@ -8,4 +8,19 @@ Template.threadpage.helpers({
     var posts = Posts.find({chosenThread: id},{sort: {createdAt: -1}});
     return posts;
   },
+
+  single_thread: function() {
+    var id = Session.get("selectedThread");
+    var single_thread = Threads.findOne( {_id: id } );
+    return single_thread;
+  },
+});
+
+
+Template.threadpage.events({
+  "click #testing": function() {
+    var id = Session.get("selectedThread");
+    console.log(id);
+  },
+
 });
